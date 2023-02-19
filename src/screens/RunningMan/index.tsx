48aa885e36083b0e7113cb4c5946ca3c73ba3ffd
Prabhat Ghimire <prabhatghimire@Prabhats-MacBook-Pro.local>
2023-02-19 13:39:35 +0545

    fix:now can be rotate by draging

--- src/screens/RunningMan/index.tsx
+++ src/screens/RunningMan/index.tsx
@@ -1,47 +1,57 @@
 import React, {useEffect, useState} from 'react';
 import ModelView from 'react-native-gl-model-view';
-import {Animated, Easing, View} from 'react-native';
+import {Animated, Easing, StyleSheet, View} from 'react-native';
 
 const AnimatedModelView = Animated.createAnimatedComponent(ModelView);
 
-// constructor() {
-//   this.state = {
-//       zoom: new Animated.Value(0),
-//       // ...
-//   };
-//   Object.keys(this.state).forEach(key =>
-//       this.state[key] instanceof Animated.Value &&
-//       this.state[key].__makeNative()
-//   );
-// }
-
 export const RunningMan = () => {
-  const [zoom, setZoom] = useState(-1);
-  // var zoom = -1;
+  const [rotateX, setRotateX] = useState(0); //useState(new Animated.Value(90));
+  const [rotateZ, setRotateZ] = useState(0); //useState(new Animated.Value(0));
+  const [fromXY, setFromXY] = useState();
+  const [valueXY, setValueXY] = useState();
 
-  // useEffect(() => {
-  //   Animated.timing(zoom, {
-  //     toValue: -2,
-  //     useNativeDriver: true,
-  //     duration: 2000,
-  //     easing: Easing.bounce,
-  //   }).start();
-  // }, []);
+  const onMoveEnd = () => {
+    setFromXY(0);
+  };
+
+  const onMove = e => {
+    let {pageX, pageY} = e.nativeEvent;
+    if (!fromXY) {
+      setFromXY([pageX, pageY]);
+      setValueXY([rotateZ, rotateX]);
+    } else {
+      setRotateZ(valueXY[0] + (pageX - fromXY[0]) / 2);
+      setRotateX(valueXY[1] + (pageY - fromXY[1]) / 2);
+    }
+  };
+
   return (
     <View style={{flex: 1}}>
       <ModelView
         model={{
-          uri: 'man.obj',
+          uri: 'running.obj',
         }}
         texture={{
           uri: 'CS.JPG',
         }}
+        onStartShouldSetResponder={() => true}
+        onResponderRelease={onMoveEnd}
+        onResponderMove={onMove}
+        animate={!!fromXY}
+        tint={{r: 1.0, g: 1.0, b: 1.0, a: 1.0}}
         scale={0.01}
-        // rotateZ={270}
-        style={{flex: 1}}
-        animate={true}
-        translateZ={zoom}
+        rotateX={rotateX}
+        rotateZ={rotateZ}
+        translateZ={-4}
+        style={styles.container}
       />
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'transparent',
+  },
+});
